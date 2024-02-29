3b95bc905b24493b1bf746f1206aad80ed240125
Alexander Francisco Villacres Guerrero <afvillacres@espe.edu.ec>
2024-02-29 12:25:27 -0500

    Update EmpleadosSuperClase.java

--- 06-Code/ProyectoMandangoMVC/src/mandango/modelo/EmpleadosSuperClase.java
+++ 06-Code/ProyectoMandangoMVC/src/mandango/modelo/EmpleadosSuperClase.java
@@ -4,10 +4,84 @@
  */
 package mandango.modelo;
 
+import java.util.Date;
+
 /**
  *
  * @author Alexander
  */
 public class EmpleadosSuperClase {
+    private String nombre, apellido, rol;
+    private Date fechaNacimiento;   
+    private String cedula, usuario, contrasenia;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public EmpleadosSuperClase() {
+    }
+
+    public EmpleadosSuperClase(String nombre, String apellido, String rol, Date fechaNacimiento, String cedula, String usuario, String contrasenia) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.rol = rol;
+        this.fechaNacimiento = fechaNacimiento;
+        this.cedula = cedula;
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+    }
     
 }
