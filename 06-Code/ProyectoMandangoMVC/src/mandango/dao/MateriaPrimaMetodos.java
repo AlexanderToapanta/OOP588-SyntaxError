a4a4372ca1491b16a65342a9800dd24be3ebd919
Alexander Jhosue Toapanta Jama <141873111+AlexanderToapanta@users.noreply.github.com>
2024-03-03 17:39:35 -0500

    update ingreso de ingredientes
    Se creo un formulario para el ingreso de ingredientes

---
+++ 06-Code/ProyectoMandangoMVC/src/mandango/dao/MateriaPrimaMetodos.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mandango.dao;
+
+import com.mongodb.MongoException;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import mandango.modelo.MateriaPrima;
+import org.bson.Document;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MateriaPrimaMetodos implements IMateriaPrima{
+    Conexion conn = new Conexion();
+ MongoDatabase database;
+  private MongoCollection coleccionMateriprima;
+
+    public MateriaPrimaMetodos() {
+        if (conn != null) {
+            this.conn = conn.crearConexion();
+            this.database = conn.getDataB();
+            this.coleccionMateriprima = database.getCollection("MateriaPrima");
+            
+            
+        }
+    }
+    private void cierreConexion(){
+       try{
+           conn.getMongo().close();
+       }catch (MongoException ex){
+           JOptionPane.showMessageDialog(null, "Error al conectar"+ex.toString());
+       }
+    }
+
+    @Override
+    public List<MateriaPrima> ListarMateriaPrima() {
+       List<MateriaPrima> listamateriaprima = new ArrayList<>();
+         FindIterable<Document> documento;
+        try{
+            documento=coleccionMateriprima.find();
+        for (Document buscar : documento){
+            
+            String nombreMateria = buscar.getString("nombreMateriaP");
+            int cantidad = buscar.getInteger("cantidad");
+            double precio = buscar.getDouble("precio");
+            MateriaPrima materia = new MateriaPrima(nombreMateria, cantidad, precio);
+            listamateriaprima.add(materia);
+        }
+        
+        
+        }catch (MongoException ex){
+            JOptionPane.showMessageDialog(null,"Error al consultar datos"+ex.getMessage());
+        }finally{
+           cierreConexion();
+        }
+        return listamateriaprima;
+    
+    }
+
+    @Override
+    public boolean InsertarMateriaPrima(MateriaPrima materia) {
+       Document documento;
+        try{
+            documento = new Document ("nombreMateriaP",materia.getNombreMateriPrima())
+                    .append("cantidad", materia.getCantidad())
+                    .append("precio", materia.getPrecio());
+            coleccionMateriprima.insertOne(documento);
+        }catch(MongoException ex){
+                    
+                    }finally{
+                            cierreConexion();
+                            }
+                    return true;
+    }
+    
+}
