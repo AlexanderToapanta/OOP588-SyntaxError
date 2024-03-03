1ce944b1cb53fa5e91747a5d81c350324ddac083
Alexander Francisco Villacres Guerrero <afvillacres@espe.edu.ec>
2024-03-02 19:11:50 -0500

    Avances

--- 06-Code/ProyectoMandangoMVC/src/mandango/vista/EmpleadosFormulario.java
+++ 06-Code/ProyectoMandangoMVC/src/mandango/vista/EmpleadosFormulario.java
@@ -20,8 +20,7 @@ public class EmpleadosFormulario extends javax.swing.JFrame {
 
    EmpleadosSuperClase empleado = new EmpleadosSuperClase();
 
-    EmpleadosSuperClase empleado = new EmpleadosSuperClase();
-
+  
     private  String dato;
     public EmpleadosFormulario() {
         initComponents();
