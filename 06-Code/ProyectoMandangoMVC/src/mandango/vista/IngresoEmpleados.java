9c014becfbb7e3bfd017b9b75b85d382adb65217
Alexander Jhosue Toapanta Jama <141873111+AlexanderToapanta@users.noreply.github.com>
2024-03-06 12:26:52 -0500

    ya creo q vale

--- 06-Code/ProyectoMandangoMVC/src/mandango/vista/IngresoEmpleados.java
+++ 06-Code/ProyectoMandangoMVC/src/mandango/vista/IngresoEmpleados.java
@@ -4,18 +4,13 @@
  */
 package mandango.vista;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 import java.text.SimpleDateFormat;
->>>>>>> parent of 49db744 (gg)
-=======
+
 import java.text.SimpleDateFormat;
->>>>>>> parent of 49db744 (gg)
-=======
+
 import java.text.SimpleDateFormat;
->>>>>>> parent of 49db744 (gg)
+
 import javax.swing.JOptionPane;
 import mandango.modelo.EmpleadosSuperClase;
 import mandango.servicio.GerenteServicio;
@@ -99,14 +94,14 @@ public class IngresoEmpleados extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel3)
                             .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
-<<<<<<< HEAD
-                            .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+
+                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 127, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(txtApellido, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(txtCedula, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE))))
-=======
+
                             .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(57, 57, 57)
@@ -117,12 +112,12 @@ public class IngresoEmpleados extends javax.swing.JFrame {
                     .addComponent(cmbRol, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtCedula, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE))
->>>>>>> parent of 49db744 (gg)
+
                 .addGap(50, 50, 50))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-<<<<<<< HEAD
+
                         .addGap(142, 142, 142)
                         .addComponent(btnIngresar)
                         .addGap(51, 51, 51)
@@ -133,7 +128,7 @@ public class IngresoEmpleados extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(177, 177, 177)
                         .addComponent(jLabel1)))
-=======
+
                         .addGap(35, 35, 35)
                         .addComponent(jLabel4)
                         .addGap(107, 107, 107)
@@ -146,7 +141,7 @@ public class IngresoEmpleados extends javax.swing.JFrame {
                         .addComponent(btnIngresar)
                         .addGap(45, 45, 45)
                         .addComponent(btnRegresar)))
->>>>>>> parent of 49db744 (gg)
+
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -165,9 +160,7 @@ public class IngresoEmpleados extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     .addComponent(txtApellido, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(30, 30, 30)
                 .addComponent(jLabel4)
@@ -180,7 +173,7 @@ public class IngresoEmpleados extends javax.swing.JFrame {
                     .addComponent(btnIngresar)
                     .addComponent(btnRegresar))
                 .addGap(59, 59, 59))
-=======
+
                     .addComponent(txtApellido, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(20, 20, 20)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -195,8 +188,8 @@ public class IngresoEmpleados extends javax.swing.JFrame {
                     .addComponent(btnIngresar)
                     .addComponent(btnRegresar))
                 .addGap(90, 90, 90))
->>>>>>> parent of 49db744 (gg)
-=======
+
+
                     .addComponent(txtApellido, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(20, 20, 20)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -211,8 +204,7 @@ public class IngresoEmpleados extends javax.swing.JFrame {
                     .addComponent(btnIngresar)
                     .addComponent(btnRegresar))
                 .addGap(90, 90, 90))
->>>>>>> parent of 49db744 (gg)
-=======
+
                     .addComponent(txtApellido, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(20, 20, 20)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -227,8 +219,7 @@ public class IngresoEmpleados extends javax.swing.JFrame {
                     .addComponent(btnIngresar)
                     .addComponent(btnRegresar))
                 .addGap(90, 90, 90))
->>>>>>> parent of 49db744 (gg)
-        );
+);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -250,31 +241,29 @@ public class IngresoEmpleados extends javax.swing.JFrame {
                 txtCedula.getText(),
                 txtNombre.getText(),
                 txtApellido.getText(),
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                 clFNacimiento.getDate(),
                 txtCedula.getText(),
                 cmbRol.getSelectedItem().toString(),
                 clFNacimiento.getDate().toString()
-=======
+
                 clFechaNacimiento.getDate(),
                 txtCedula.getText(),
                 cmbRol.getSelectedItem().toString(),
                 formatoFecha.format(clFechaNacimiento.getDate())
->>>>>>> parent of 49db744 (gg)
-=======
+
+
                 clFechaNacimiento.getDate(),
                 txtCedula.getText(),
                 cmbRol.getSelectedItem().toString(),
                 formatoFecha.format(clFechaNacimiento.getDate())
->>>>>>> parent of 49db744 (gg)
-=======
+
+
                 clFechaNacimiento.getDate(),
                 txtCedula.getText(),
                 cmbRol.getSelectedItem().toString(),
                 formatoFecha.format(clFechaNacimiento.getDate())
->>>>>>> parent of 49db744 (gg)
+
                 );
         if(GerenteServicio.InsertarEmpleado(insertarEmpleado)){
             JOptionPane.showMessageDialog(null, "registro ingresado correctamente");
@@ -329,18 +318,13 @@ public class IngresoEmpleados extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnIngresar;
     private javax.swing.JButton btnRegresar;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
     private com.toedter.calendar.JDateChooser clFechaNacimiento;
->>>>>>> parent of 49db744 (gg)
-=======
+
     private com.toedter.calendar.JDateChooser clFechaNacimiento;
->>>>>>> parent of 49db744 (gg)
-=======
+
     private com.toedter.calendar.JDateChooser clFechaNacimiento;
->>>>>>> parent of 49db744 (gg)
+
     private javax.swing.JComboBox<String> cmbRol;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
